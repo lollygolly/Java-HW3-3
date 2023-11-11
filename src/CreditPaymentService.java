0a08ca24560b28906dc4513ab99e9d16c0eec5f8
LollyGolly <valentina_96@inbox.ru>
2023-11-12 00:58:24 +0500

    code reformed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,10 @@
 public class CreditPaymentService {
     public int calculate(double c, double m, double s) {
+        // Считаем коэффициент аннуитета:
         double k = ((m / 12 / 100) * Math.pow(1 + (m / 12 / 100), s * 12)) / (Math.pow(1 + (m / 12 / 100), s * 12) - 1);
-        double x = c * k;
-        int payMent;
-        return payMent = (int) x;
+        double x = c * k; // Рассчитываем аннуитентный ежемесячный платеж
+
+        int payMent; // Вводим значение ежемесячного платежа при заданных значениях суммы, срока кредита и годовой ставки по кредиту
+        return payMent = (int) x; // Вывод искомого значения ежемесячного платежа по кредиту
     }
 }
